cb7212205e8a935817a1563ab4fdcb0e99760ccd
RisinaR <rkarisina@gmail.com>
2022-08-29 22:46:28 +0530

    SideNav Bar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import Home from './components/Home';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Employee from "./components/Employee";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employee" element={<Employee />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
